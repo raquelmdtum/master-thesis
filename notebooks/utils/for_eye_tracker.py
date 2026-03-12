c8780505e929821b7753fadffec1fd6d10fe231c
raquelmdtum <s243636@dtu.dk>
2026-03-12 16:38:03 +0100

    eye tracking processing done ?

--- notebooks/utils/for_eye_tracker.py
+++ notebooks/utils/for_eye_tracker.py
@@ -11,17 +11,17 @@ import os
 
 def export_gaze_to_csv(dataset, outdir):
 
-    gaze_timestamps = dataset.streams.PupilLabs.Counter.Gaze.data
-    #gaze_timestamps.reset_index(inplace = T1rue)
-    gaze_data = dataset.streams.PupilLabs.PupilGaze.data
-    gaze = gaze_timestamps.join(gaze_data, on='Value') 
-    gaze = gaze.drop('Value', axis=1)
-    video_frames = dataset.streams.PupilLabs.Counter.DecodedFrames.data
-    video_frames = video_frames[video_frames.Value !=0]
-    #eyetracking_correlation = pd.merge_asof(data, space_time, left_index=True, right_index=True)
-    gaze_coorelation = pd.merge_asof(video_frames, gaze, left_index=True, right_index=True)
-    gaze_coorelation.to_csv(os.path.join(outdir,'gaze.csv'))
+    # Pupil Gaze data removed because not existent
+    # gaze_timestamps = dataset.streams.PupilLabs.Counter.Gaze.data
 
+    gaze_data = dataset.streams.PupilLabs.PupilGaze.data
+
+    video_frames = dataset.streams.PupilLabs.DecodedFrames.data
+    video_frames = video_frames[video_frames.Value != 0]
+
+    gaze_correlation = pd.merge_asof(video_frames,gaze_data,left_index=True,right_index=True)
+
+    gaze_correlation.to_csv(os.path.join(outdir, 'gaze.csv'))
 
 def export_gaze_andre(dataset):
 
